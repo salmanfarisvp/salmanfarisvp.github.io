799f4608499078b33be94e8fe660e2bc55320138
Salman Faris <farissalmannbr@gmail.com>
2025-07-11 08:58:49 +0530

    fix button ui on small screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons} style={{display: 'flex', gap: '10px'}}>
+        <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="https://github.com/salmanfarisvp">
